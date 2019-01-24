53bae1f472ce10c1165cd4b9a2d2b5500ff72061
Daniel Moore <dmoore@shearwater.com.au>
2019-01-24 04:02:13 +0000

    reacreating deleted program

---
+++ analyse_passwords.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main(void) {
+    int NUM_PASSWORDS = 1000;
+    int passwords[NUM_PASSWORDS];
+    
+    read_input(passwords, NUM_PASSWORDS);
+    print_int_arr(passwords, NUM_PASSWORDS);
+    return 0;
+}
+
+void read_input(int arr[], int n) {
+    for(int i=0 ; i<n ; i++) {
+        scanf("%X", &arr[i]);
+    }
+}
+
+void print_int_arr(int arr[], int n) {
+    for(int i=0 ; i<n ; i++) {
+        printf("%X\n", arr[i]);
+    }
+}
+
+// A utility function to swap two elements
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+    array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+int partition (int arr[], int low, int high) {
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+
+    for (int j = low; j <= high- 1; j++) {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot) {
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,
+  low  --> Starting index,
+  high  --> Ending index */
+void quick_sort(int arr[], int low, int high) {
+    if (low < high) {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi = partition(arr, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+} 
+
